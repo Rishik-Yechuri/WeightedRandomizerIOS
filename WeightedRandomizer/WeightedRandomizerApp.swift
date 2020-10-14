3972b01df4c4e8188ac6ef16656dab4b58a1fec2
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-10-14 15:40:27 -0700

    Initial Commit

---
+++ WeightedRandomizer/WeightedRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightedRandomizerApp.swift
+//  WeightedRandomizer
+//
+//  Created by Rishik Yechuri on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct WeightedRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
