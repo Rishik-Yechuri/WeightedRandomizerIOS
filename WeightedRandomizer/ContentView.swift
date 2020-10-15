8e98dd9dfb43c45889d0249d251cd4674d820196
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-10-14 20:40:49 -0700

    Update ContentView.swift

--- WeightedRandomizer/ContentView.swift
+++ WeightedRandomizer/ContentView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numOfGroups: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Spacer()
+                Button(action: {}) {
+                    Text("Hey")
+                }.offset(x:-20,y:0)
+            }
+            Spacer()
+            VStack{
+                Text("# of groups")
+                HStack{
+                    Spacer()
+                    TextField("Ex:3", text: $numOfGroups)
+                        .frame(width: 80.0)
+                        .textFieldStyle(RoundedBorderTextFieldStyle ())
+                    Spacer ()
+                }
+            }
+            Spacer()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct   ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
